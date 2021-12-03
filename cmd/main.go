c1eecf7f8326455a221e0599deb2ff3341efd087
Ljudevit Ocic <ljudevit@trustap.com>
2021-12-03 14:19:22 +0100

    First commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,43 @@
-package cmd
+package main
+
+import (
+	"github.com/divilla/events.git"
+	"log"
+)
+
+type (
+	One struct {
+		Id   int
+		Name string
+	}
+)
+
+func main() {
+	e := events.NewEventsManager(log.Default())
+
+	e.Subscribe("one-create", func(target interface{}, data events.Map) error {
+		t := target.(*One)
+		println(t.Id, t.Name)
+		println(data["some-data"].(string))
+		data["other-data"] = "bbb"
+
+		return nil
+	})
+
+	e.Subscribe("one-create", func(target interface{}, data events.Map) error {
+		t := target.(*One)
+		println(t.Id, t.Name)
+		println(data["other-data"].(string))
+
+		return nil
+	})
+
+	one := &One{
+		Id:   1,
+		Name: "One",
+	}
+
+	if err := e.Dispatch("one-create", one, events.Map{"some-data": "aaa"}); err != nil {
+		log.Fatal(err)
+	}
+}
--- events.go
+++ events.go
@@ -1,13 +1,61 @@
 package events
 
+import (
+	"errors"
+	"sync"
+)
+
+var ErrMissingKey = errors.New("key not found")
+
 type (
+	Logger interface {
+		Fatal(v ...interface{})
+		Fatalf(format string, v ...interface{})
+	}
+
 	Map map[string]interface{}
 
-	events struct {
+	Handler func(target interface{}, data Map) error
 
+	Events struct {
+		hm  map[string][]Handler
+		log Logger
+		rwm *sync.RWMutex
 	}
 )
 
-func Handler(target interface{}, data Map) error {
+func NewEventsManager(logger Logger) *Events {
+	return &Events{
+		hm: make(map[string][]Handler),
+		log: logger,
+		rwm: new(sync.RWMutex),
+	}
+}
 
+func (e *Events) Subscribe(key string, handler Handler)  {
+	e.rwm.Lock()
+	defer e.rwm.Unlock()
+
+	if _, ok := e.hm[key]; !ok {
+		e.hm[key] = []Handler{}
+	}
+
+	e.hm[key] = append(e.hm[key], handler)
+}
+
+func (e *Events) Dispatch(key string, target interface{}, data Map) error {
+	e.rwm.RLock()
+	defer e.rwm.RUnlock()
+
+	if _, ok := e.hm[key]; !ok {
+		return ErrMissingKey
+	}
+
+	for _, handler := range e.hm[key] {
+		if err := handler(target, data); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
