395839cc33792e5e64f5040a166aca10c5bea6ac
Ljudevit Ocic <ljudevit@trustap.com>
2021-12-03 14:16:02 +0100

    First commit

---
+++ events.go
@@ -0,0 +1,13 @@
+package events
+
+type (
+	Map map[string]interface{}
+
+	events struct {
+
+	}
+)
+
+func Handler(target interface{}, data Map) error {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/divilla/events.git
+
+go 1.16
